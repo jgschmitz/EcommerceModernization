b22a5276eca6a0e2efe79c830b6a33800c579bce
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2024-01-17 18:11:14 -0600

    Create CurlVector.bash

---
+++ CurlVector.bash
@@ -0,0 +1,4 @@
+curl https://chicagofireopenai.openai.azure.com/openai/deployments/ChicagoFireTextEmbedding/embeddings\?api-version\=2023-05-15\
+  -H 'Content-Type: application/json' \
+  -H 'api-key: a7e225a5012c45a4bd2a530360e00507' \
+  -d '{"input": "Sample Document goes here"}'
